24cb5f28701a87091541e951d98a63dd63834ce6
noyonalways <noyonrahman2003@gmail.com>
2024-01-31 20:58:33 +0600

    prblem-1: function return type changed

--- problem-1.ts
+++ problem-1.ts
@@ -1,11 +1,10 @@
 // Design a TypeScript function that takes a parameter of a union type (e.g., string | number). If it's a string, return its length. If it's a number, return the square of that number.
 
-function squareOrLength(x: string | number): number | string {
-  if (typeof x === "string") {
-    return x.length;
-  } else {
-    return x * x;
+function squareOrLength(inputValue: string | number): number {
+  if (typeof inputValue === "string") {
+    return inputValue.length;
   }
+  return inputValue * inputValue;
 }
 
 console.log("String:", squareOrLength("Noyon"));
