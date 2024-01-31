ad3b27194ad96bfc39f2dae9ba87a577ad32f9e9
noyonalways <noyonrahman2003@gmail.com>
2024-01-31 20:47:44 +0600

    refactor: problem 2 refactored

--- problem-2.ts
+++ problem-2.ts
@@ -1,23 +1,35 @@
 // Create an interface called Person with optional properties address and phone. The address property itself will be another object containing city and street properties. Implement a function named getAddressCity that takes an argument of type Person and returns the city from the address property of the Person object. Use optional chaining to prevent any type errors.
 
+interface Address {
+  city: string;
+  street: string;
+}
+
 interface Person {
-  address?: {
-    city: string;
-    street: string;
-  };
+  address?: Address;
   phone?: string;
 }
 
 function getAddressCity(person: Person): string {
-  return person.address?.city as string;
+  const city = person?.address?.city;
+  if (city) {
+    return city;
+  } else {
+    return "No address provided";
+  }
 }
 
-const devNoyon = {
+const person1: Person = {
+  phone: "+8801706592962",
   address: {
     city: "Gazipur, Dhaka",
-    street: "Ranigonj Bazar, Fulbaria Road",
+    street: "Ranigonj to Fulbaria",
   },
-  phone: "+8801706592962",
 };
 
-console.log(getAddressCity(devNoyon));
+const person2: Person = {
+  phone: "+8801401234567",
+};
+
+console.log(getAddressCity(person1)); // Gazipur, Dhaka
+console.log(getAddressCity(person2)); // No address provided
