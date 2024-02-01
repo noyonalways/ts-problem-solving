b0d75d36fde033d85a704a924d14a42767753e5f
noyonalways <noyonrahman2003@gmail.com>
2024-02-01 17:33:55 +0600

    problem-5 solved

---
+++ problem-5.ts
@@ -0,0 +1,33 @@
+interface Car {
+  make: string;
+  model: string;
+  year: number;
+}
+
+interface Driver {
+  name: string;
+  licenseNumber: number;
+}
+
+function combineCarAndDriver(
+  car: Car,
+  driver: Driver
+): { car: Car; driver: Driver } {
+  return {
+    car,
+    driver,
+  };
+}
+
+const car: Car = {
+  make: "Honda",
+  model: "Civic",
+  year: 2020,
+};
+
+const driver: Driver = {
+  name: "Noyon Rahman",
+  licenseNumber: 123456789,
+};
+
+console.log(combineCarAndDriver(car, driver));
